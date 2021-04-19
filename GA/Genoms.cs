4722e3d099b2547fb08f0d7ba35a9219c9de729b
Lucas Corsi <lucasc@pbtech.net.br>
2021-04-19 17:54:12 -0300

    Add project files.

---
+++ GA/Genoms.cs
@@ -0,0 +1,32 @@
+﻿using GeneticSharp.Domain.Chromosomes;
+
+namespace GeneticAlgorithmWords.GA
+{
+    public sealed class Genoms : ChromosomeBase
+    {
+        public  string genoms = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890, .-;:_!\"#%&/()=?@${[]}";
+        public string _target;
+        public Genoms(string target) : base(100)
+        {
+            _target = target;
+            CreateGenes();
+        }
+
+        public override Gene GenerateGene(int geneIndex)
+        {
+            var characteresTarget = _target.ToCharArray();
+            var characteresGenom = genoms.ToCharArray();
+            var gnome = string.Empty;
+            for (var i = 0; i < characteresTarget.Length; i++)
+            {
+                gnome += characteresGenom[i];
+            }
+            return new Gene(gnome);
+        }
+
+        public override IChromosome CreateNew()
+        {
+            return new Genoms(_target);
+        }
+    }
+}
---
+++ GA/MyFitness.cs
@@ -0,0 +1,22 @@
+﻿using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Fitnesses;
+
+namespace GeneticAlgorithmWords.GA
+{
+    public class MyFitness : IFitness
+    {
+        public double Evaluate(IChromosome chromosome)
+        {
+            var gene = chromosome.GetGene(0);
+            var characteresTarget = Genoms._target.ToCharArray();
+            var characteresGenom = gene.Value.ToString()?.ToCharArray();
+            double fitness = 0;
+            for (var i = 0; i < characteresTarget.Length; i++)
+            {
+                if (characteresGenom != null && characteresGenom[i] != characteresTarget[i])
+                    fitness++;
+            }
+            return fitness;
+        }
+    }
+}
