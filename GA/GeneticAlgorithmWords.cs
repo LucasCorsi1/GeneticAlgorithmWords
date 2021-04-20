861e51e18d5d4512e7bd6426795f11befeb5a8f4
Lucas Corsi <lucasc@pbtech.net.br>
2021-04-20 15:56:02 -0300

    Corrige falhas de implementação

--- GA/GeneticAlgorithmWords.cs
+++ GA/GeneticAlgorithmWords.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Threading;
 using GeneticSharp.Domain;
+using GeneticSharp.Domain.Chromosomes;
 using GeneticSharp.Domain.Crossovers;
 using GeneticSharp.Domain.Mutations;
 using GeneticSharp.Domain.Populations;
@@ -19,10 +20,11 @@ namespace GeneticAlgorithmWords.GA
         private Timer _timer;
 
         private Stopwatch _stopwatch;
+        private bool IsRunning => _timer != null;
 
         public double Fitness => _ga != null && _ga.BestChromosome?.Fitness != null ? _ga.BestChromosome.Fitness.Value : 0;
         public int GenerationsNumber => _ga != null ? _ga.GenerationsNumber : 0;
-
+        public Gene[] Genes => _ga?.BestChromosome?.GetGenes();
         public string Counter => _stopwatch != null ? _stopwatch.Elapsed.ToString(@"\.hh\:mm\:ss") : "00:00:00";
 
         public void StartGeneticAlgo(string target)
@@ -34,22 +36,37 @@ namespace GeneticAlgorithmWords.GA
             var chromosome = new Chromosome(target);
             var population = new Population(500, 1000, chromosome);
             _stopwatch = Stopwatch.StartNew();
+
             _ga = new GeneticAlgorithm(population, fitness, selection, crossover, mutation)
             {
                 Termination = new GenerationNumberTermination(300)
             };
             _timer = new Timer(new TimerCallback(_ =>
             {
-               
                 _ga.Termination = new GenerationNumberTermination(_ga.GenerationsNumber + 1);
-
                 if (_ga.GenerationsNumber > 0)
                     _ga.Resume();
                 else
                     _ga.Start();
 
+                var teste = string.Empty;
+                for (var i = 0; i < _ga.BestChromosome.GetGenes().Length; i++)
+                {
+                    teste += _ga.BestChromosome.GetGenes()[i].Value;
+                    if (teste == target)
+                        Stop();
+                }
                 StateHasChanged?.Invoke();
             }), null, 0, 1);
         }
+
+        private void Stop()
+        {
+            if (!IsRunning) return;
+            _timer.Dispose();
+            _timer = null;
+            _ga.Stop();
+        }
+
     }
 }
--- GA/MyFitness.cs
+++ GA/MyFitness.cs
@@ -7,18 +7,15 @@ namespace GeneticAlgorithmWords.GA
     public class MyFitness : IFitness
     {
         public string Target { get; set; }
-        public string CurrentTarget { get; set; }
 
         public double Evaluate(IChromosome chromosome)
         {
             var characteresTarget = Target.ToCharArray();
             var genes = chromosome.GetGenes();
-            int count = 0;
+            var count = 0;
             double fitness = 0;
-
             foreach (var gene in genes)
             {
-                CurrentTarget = gene.Value.ToString();
                 if (Convert.ToChar(gene.Value) == characteresTarget[count++])
                     fitness++;
             }
